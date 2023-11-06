615c7339b0606d888ff23beab55273e0151fa65b
GATA5 <Tangjing.Gan@coop.ch>
2023-11-06 09:48:01 +0100

    THEN

--- src/test/java/Test2D.java
+++ src/test/java/Test2D.java
@@ -1,5 +1,17 @@
 public class Test2D {
     public static void main(String[] args){
 
+        @Test
+        void playerCharacterTest_whengetx_thenReturn0() {
+
+            //GIVEN
+            int number = 0;
+
+            //WHEN
+            int actual = Test2D.Test(number);
+
+            //THEN
+            AssertEquals(expected:0, actual);
+        }
     }
 }
